90c565b81d765cdd11e01b8fcc78395356514e89
吉井駿一 <syun_syun01@icloud.com>
2021-02-01 02:00:06 +0900

    vue化

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,110 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="container">
+    <h1 id="kill">殺すぞ</h1>
+    <div id="tweet">
+      <a
+        href="http://twitter.com/share?url=https://yoshiishunichi.github.io/kill/&text=殺すぞ"
+        id="tweetbutton"
+        target="_blank"
+        >ツイートする</a
+      >
+      <input id="start" type="button" value="スタートっ！" @click="tapStart" />
+      <input id="stop" type="button" value="ストップっ！" @click="tapStop" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      timeInterval: null
+    };
+  },
+  methods: {
+    tapStart() {},
+    tapStop() {}
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  margin: 0px;
+  padding: 0px;
+}
+
+body {
+  background-color: black;
+}
+
+div {
+  height: 100%;
+  width: 100%;
+  margin: 0 auto;
+}
+
+#tweet {
+  width: 90%;
+  margin: 0 auto;
+}
+
+h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 100px;
+  padding-bottom: 100px;
+  color: white;
+  font-size: 50px;
+  user-select: none;
+}
+
+#tweetbutton {
+  display: inline-block;
+  color: white;
+  font-weight: bold;
+  font-size: 15px;
+  background-color: skyblue;
+  border: 1px gray solid;
+  text-decoration: none;
+  width: 120px;
+  height: 28px;
+  text-align: center;
+  user-select: none;
+  vertical-align: middle;
+  border-radius: 5px;
+  line-height: 30px;
+}
+
+#start {
+  display: inline-block;
+  background-color: #eee;
+  color: black;
+  margin-left: 30px;
+  font-weight: bold;
+  cursor: pointer;
+  text-align: center;
+  width: 100px;
+  height: 30px;
+  border-radius: 5px;
+  border: 1px gray solid;
+  font-size: 14px;
+  outline: none;
+}
+
+#stop {
+  display: none;
+  background-color: #eee;
+  color: black;
+  margin-left: 30px;
+  font-weight: bold;
+  cursor: pointer;
+  text-align: center;
+  width: 100px;
+  height: 30px;
+  border-radius: 5px;
+  border: 1px gray solid;
+  font-size: 14px;
+  outline: none;
 }
 </style>
