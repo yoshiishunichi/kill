2594ade8ebca2a0c31fbdee64221d5de359838f3
吉井駿一 <syun_syun01@icloud.com>
2021-02-01 02:12:05 +0900

    ほぼ完成っぽい

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,20 @@
         target="_blank"
         >ツイートする</a
       >
-      <input id="start" type="button" value="スタートっ！" @click="tapStart" />
-      <input id="stop" type="button" value="ストップっ！" @click="tapStop" />
+      <input
+        id="start"
+        type="button"
+        value="スタートっ！"
+        :class="{ disp: disps[0], nodisp: nodisps[0] }"
+        @click="tapStart"
+      />
+      <input
+        id="stop"
+        type="button"
+        value="ストップっ！"
+        :class="{ disp: disps[1], nodisp: nodisps[1] }"
+        @click="tapStop"
+      />
     </div>
   </div>
 </template>
@@ -19,12 +31,48 @@ export default {
   name: "App",
   data() {
     return {
-      timeInterval: null
+      timeInterval: null,
+      disps: [true, false],
+      nodisps: [false, true]
     };
   },
   methods: {
-    tapStart() {},
-    tapStop() {}
+    colorChange() {
+      let colors = [0, 0, 0];
+      for (let i = 0; i < colors.length; i++) {
+        colors[i] = Math.floor(Math.random() * 256);
+      }
+
+      const thisColor =
+        "rgb(" +
+        String(colors[0]) +
+        ", " +
+        String(colors[1]) +
+        ", " +
+        String(colors[2]) +
+        ")";
+      return thisColor;
+    },
+    setColor() {
+      const kill = document.getElementById("kill");
+      const container = document.body;
+      kill.style.color = this.colorChange();
+      container.style.backgroundColor = this.colorChange();
+    },
+    tapStart() {
+      this.disps[0] = false;
+      this.disps[1] = true;
+      this.nodisps[0] = true;
+      this.nodisps[1] = false;
+      this.timeInterval = setInterval(this.setColor, 100);
+    },
+    tapStop() {
+      this.disps[1] = false;
+      this.disps[0] = true;
+      this.nodisps[1] = true;
+      this.nodisps[0] = false;
+      clearInterval(this.timeInterval);
+    }
   }
 };
 </script>
@@ -76,15 +124,22 @@ h1 {
   line-height: 30px;
 }
 
-#start {
+.disp {
   display: inline-block;
+}
+
+.nodisp {
+  display: none;
+}
+
+#start {
   background-color: #eee;
   color: black;
   margin-left: 30px;
   font-weight: bold;
   cursor: pointer;
   text-align: center;
-  width: 100px;
+  width: 120px;
   height: 30px;
   border-radius: 5px;
   border: 1px gray solid;
@@ -93,14 +148,13 @@ h1 {
 }
 
 #stop {
-  display: none;
   background-color: #eee;
   color: black;
   margin-left: 30px;
   font-weight: bold;
   cursor: pointer;
   text-align: center;
-  width: 100px;
+  width: 120px;
   height: 30px;
   border-radius: 5px;
   border: 1px gray solid;
